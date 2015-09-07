e33618a95932eff001bc880c371ba647a37b4ef2
saramorgan <saragit@saramorgan.net>
2015-09-07 10:11:41 -0500

    initial commit

---
+++ PetSuppliesRelationalSvc/WcfDataService1.svc.cs
@@ -0,0 +1,28 @@
+//------------------------------------------------------------------------------
+// <copyright file="WebDataService.svc.cs" company="Microsoft">
+//     Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Data.Services;
+using System.Data.Services.Common;
+using System.Linq;
+using System.ServiceModel.Web;
+using System.Web;
+
+namespace PetSuppliesRelationalSvc
+{
+    public class WcfDataService1 : DataService< /* TODO: put your data source class name here */ >
+    {
+        // This method is called only once to initialize service-wide policies.
+        public static void InitializeService(DataServiceConfiguration config)
+        {
+            // TODO: set rules to indicate which entity sets and service operations are visible, updatable, etc.
+            // Examples:
+            // config.SetEntitySetAccessRule("MyEntityset", EntitySetRights.AllRead);
+            // config.SetServiceOperationAccessRule("MyServiceOperation", ServiceOperationRights.All);
+            config.DataServiceBehavior.MaxProtocolVersion = DataServiceProtocolVersion.V3;
+        }
+    }
+}
